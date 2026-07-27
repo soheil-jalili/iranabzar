b881d88816acbc660ee3d34d38844cb51044d7aa
soheil-jalili <jalilisoheil14@gmail.com>
2026-07-27 05:08:59 -0700

    add product item

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Product from "@/components/modules/product/Product";
 import CarouselHeader from "@/components/templates/Index/CarouselHeader";
 import Features from "@/components/templates/Index/Features";
 
@@ -5,6 +6,40 @@ const Index: React.FC = () => {
   return (
     <main>
       <CarouselHeader />
+
+      <section className="container">
+        <div className="flex items-center gap-[52px] mb-34.25">
+          <Product
+            title="کارواش رونیکس U100E"
+            description="ronix 2211 impact drill"
+            price={1_580_000}
+            backgroundColor="bg-product-detail-container2-color"
+            image="/assets/images/chainsaw.png"
+          />
+          <Product
+            title="کارواش رونیکس U100E"
+            description="ronix 2211 impact drill"
+            price={1_580_000}
+            backgroundColor="bg-product-detail-container2-color"
+            image="/assets/images/chainsaw.png"
+          />
+          <Product
+            title="کارواش رونیکس U100E"
+            description="ronix 2211 impact drill"
+            price={1_580_000}
+            backgroundColor="bg-product-detail-container2-color"
+            image="/assets/images/chainsaw.png"
+          />
+          <Product
+            title="کارواش رونیکس U100E"
+            description="ronix 2211 impact drill"
+            price={1_580_000}
+            backgroundColor="bg-product-detail-container2-color"
+            image="/assets/images/chainsaw.png"
+          />
+        </div>
+      </section>
+
       <Features />
     </main>
   );
